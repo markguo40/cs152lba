aa6f030c036dc5923f678ae56962cd4bd2ded287
zhanchen guo <markguo@minerva.kgi.edu>
2018-12-16 12:59:20 +0100

    create app

---
+++ restaurants/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def home(request):
+	return render(request, "index.html", {})
+
+
+def query_restaurant(request):
+	distance = request.POST.get('distance')
+	price = request.POST.get('price')
+	type_restaurant = request.POST.get('type')
+	ifveg = request.POST.get('veg')
+
+	print(distance);
+	print(price);
+	print(type_restaurant);
+	print(ifveg);
+	# Write code here: 
+
+	result = {	
+		"restaurant_name": "None" #change value here
+	} 
+	return JsonResponse(result)
