a55c4f96234acc91950d0082baa50b6734696d77
Guillaume Picard <gwpicard@gwpmac.local>
2018-12-18 13:14:19 +0000

    updated KB to still suggest restaurants with veg options if veg options is not selected

--- static/KB.pl
+++ static/KB.pl
@@ -1,13 +1,13 @@
-recommendation('Wing Wing') :- veg_options(yes), distance(far), type(korean), price(high).
-recommendation('Indian Veg') :- veg_options(yes), distance(medium), type(indian), price(low).
-recommendation('King of Falafel') :- veg_options(yes), distance(close), type(arabic), price(low).
+recommendation('Wing Wing') :- distance(far), type(korean), price(high).
+recommendation('Indian Veg') :- distance(medium), type(indian), price(low).
+recommendation('King of Falafel') :- distance(close), type(arabic), price(low).
 recommendation('Master Superfish') :- veg_options(no), distance(medium), type(british), price(medium).
 recommendation('Fryers Delight') :- veg_options(no), distance(close), type(british), price(medium).
-recommendation('Pret A Manger') :- veg_options(yes), distance(close), type(french), price(high).
-recommendation('Pizza Pilgrims') :- veg_options(yes), distance(close), type(italian), price(medium).
-recommendation('Bento Bab') :- veg_options(yes), distance(close), type(korean), price(low).
-recommendation('Itsu') :- veg_options(yes), distance(far), type(chinese), price(medium).
+recommendation('Pret A Manger') :- distance(close), type(french), price(high).
+recommendation('Wedge') :- distance(close), type(italian), price(medium).
+recommendation('Pizza Pilgrims') :- distance(close), type(italian), price(medium).
+recommendation('Bento Bab') :- distance(close), type(korean), price(low).
+recommendation('Itsu') :- distance(far), type(chinese), price(medium).
 recommendation('Great') :- veg_options(no), distance(close), type(mediterranean), price(high).
-recommendation('Wedge') :- veg_options(yes), distance(close), type(italian), price(medium).
-recommendation('Coin Laundry') :- veg_options(yes), distance(close), type(british), price(medium).
-recommendation('Leather Lane') :- veg_options(yes), distance(close), price(low).
+recommendation('Coin Laundry') :- distance(close), type(british), price(medium).
+recommendation('Leather Lane') :- distance(close), price(low).
